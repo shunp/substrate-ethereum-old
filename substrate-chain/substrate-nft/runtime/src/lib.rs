8a47e74ffe170913effc5d9d9403cfaffc1a1cfe
Shunpei Koike <shunpei42ba@gmail.com>
2019-10-16 09:52:26 +0900

    remove template files

--- substrate-chain/substrate-nft/runtime/src/lib.rs
+++ substrate-chain/substrate-nft/runtime/src/lib.rs
@@ -54,8 +54,8 @@ pub type BlockNumber = u64;
 /// Index of an account's extrinsic in the chain.
 pub type Nonce = u64;
 
-/// Used for the module template in `./template.rs`
-mod template;
+/// Used for the module template in `./nft.rs`
+mod nft;
 
 /// Opaque types. These are used by the CLI to instantiate machinery that don't need to know
 /// the specifics of the runtime. They can then be made to be agnostic over specific formats
@@ -186,15 +186,15 @@ impl sudo::Trait for Runtime {
 	type Proposal = Call;
 }
 
-/// Used for the module template in `./template.rs`
-impl template::Trait for Runtime {
+/// Used for the module template in `./nft.rs`
+impl nft::Trait for Runtime {
 	type Event = Event;
 }
 
-/// Used for the module template in `./template.rs`
-impl substrate_module_template::Trait for Runtime {
-	type Event = Event;
-}
+// /// Used for the module template in `./template.rs`
+// impl substrate_module_template::Trait for Runtime {
+// 	type Event = Event;
+// }
 
 construct_runtime!(
 	pub enum Runtime with Log(InternalLog: DigestItem<Hash, AuthorityId, AuthoritySignature>) where
@@ -210,8 +210,8 @@ construct_runtime!(
 		Balances: balances,
 		Sudo: sudo,
 		// Used for the module template in `./template.rs`
-		TemplateModule: template::{Module, Call, Storage, Event<T>},
-		ExampleModule: substrate_module_template::{Module, Call, Storage, Event<T>},
+		Nft: nft::{Module, Call, Storage, Event<T>},
+		// ExampleModule: substrate_module_template::{Module, Call, Storage, Event<T>},
 
 	}
 );
